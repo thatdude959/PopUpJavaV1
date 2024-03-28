4141fbdb64dd044d38e5baf54fff4bc07e71101e
thatdude959 <christophercheung6@gmail.com>
2024-03-28 11:28:47 -0400

    first commit

---
+++ src/mainFrame.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+// import jvax class package - window constants
+
+public class mainFrame extends JFrame {
+    public void init(){
+        setTitle("POPUP");
+        setSize(600,300 );
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+    
+}
